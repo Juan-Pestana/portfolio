a732b066b4f71f94239e3882f512aae8d5b5db11
Juan Pestana <juan.pestana82@gmail.com>
2024-06-06 22:42:35 +0200

    test Iframe

---
+++ src/components/blogComponents/react/CommentsIframe.tsx
@@ -0,0 +1,12 @@
+export default function CommentsIframe() {
+  return (
+    <>
+      <div className="mt-24 w-full h-94">
+        <iframe
+          className="w-full h-full"
+          src="https://mai-comments.vercel.app/"
+        ></iframe>
+      </div>
+    </>
+  )
+}
