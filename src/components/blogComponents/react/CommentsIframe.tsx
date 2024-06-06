af626eb4bbd65e117bde5dd953545d4f1d90b2f1
Juan Pestana <juan.pestana82@gmail.com>
2024-06-07 00:12:13 +0200

    removing comments

--- src/components/blogComponents/react/CommentsIframe.tsx
+++ src/components/blogComponents/react/CommentsIframe.tsx
@@ -1,10 +1,29 @@
+import { useRef, useState } from 'react'
+
 export default function CommentsIframe() {
+  // const ref = useRef<HTMLIFrameElement>()
+  // const [height, setHeight] = useState('0px')
+  // const onLoad = () => {
+  //   //@ts-ignore
+  //   setHeight(ref.current.contentWindow.document.body.scrollHeight + 'px')
+  // }
   return (
     <>
+      {' '}
+      {/* <button onClick={handleClick}>click{someState.count}</button> */}
       <div className="mt-24 w-full h-94">
         <iframe
-          className="w-full h-full"
-          src="https://mai-comments.vercel.app/"
+          //@ts-ignore
+
+          id="myFrame"
+          src="http://mai-comments.vercel.app/"
+          width="100%"
+          height={'1000px'}
+          style={{
+            maxWidth: 640,
+            width: '100%',
+            overflow: 'auto',
+          }}
         ></iframe>
       </div>
     </>
